ee4c8a99ad15ec1bc6b5ab43f0e653602c0068c0
Anthony Nguyen <anthonyhungnguyen276@gmail.com>
2020-05-03 12:26:46 +0700

    initial release

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,14 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+var express = require('express')
+var router = express.Router()
+const fetch = require('node-fetch')
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+/* GET home page. */
+router.get('/', async (req, res, next) => {
+	await fetch(
+		'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=10.748127710852733,106.6128096443417&radius=500&types=food&key=AIzaSyDmbl-UzpILeyTFM5_UbvCRiLHa5-6yhpU'
+	)
+		.then((res) => res.json())
+		.then((data) => res.send(data))
+})
+
+module.exports = router
