2c348b41b8abb8053e7493614eb65c0ca7b481b9
PhucHung <anthonyhungnguyen276@gmail.com>
2020-08-06 14:20:10 +0700

    fix alert error

--- components/Chat/Chat.js
+++ components/Chat/Chat.js
@@ -1,77 +1,68 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
 
-import Messages from "./Messages/Messages";
-import InfoBar from "./InfoBar/InfoBar";
-import Input from "./Input/Input";
+import Messages from './Messages/Messages'
+import InfoBar from './InfoBar/InfoBar'
+import Input from './Input/Input'
 
-import "./Chat.css";
+import './Chat.css'
 
 const Chat = ({ room, name, socket }) => {
-  const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState([]);
-  useEffect(() => {
-    socket.emit("join", { name, room }, (error) => {
-      if (error) {
-        console.log(error);
-        alert(error.error);
-      }
-    });
-  }, []);
+	const [ message, setMessage ] = useState('')
+	const [ messages, setMessages ] = useState([])
+	useEffect(() => {
+		socket.emit('join', { name, room }, (error) => {
+			if (error) {
+				if (alert(error.error)) {
+					window.location.reload()
+				}
+			}
+		})
+	}, [])
 
-  useEffect(() => {
-    const oldMessages = JSON.parse(localStorage.getItem("oldMessages"));
-    if (oldMessages) {
-      if (oldMessages[room]) {
-        setMessages(oldMessages[room]);
-      }
-    }
+	useEffect(() => {
+		const oldMessages = JSON.parse(localStorage.getItem('oldMessages'))
+		if (oldMessages) {
+			if (oldMessages[room]) {
+				setMessages(oldMessages[room])
+			}
+		}
 
-    socket.on("message", (message) => {
-      setMessages((messages) => {
-        let newMessage = [...messages, message];
-        const oldMessages = JSON.parse(localStorage.getItem("oldMessages"));
-        if (oldMessages) {
-          localStorage.setItem(
-            "oldMessages",
-            JSON.stringify({ ...oldMessages, [room]: newMessage })
-          );
-          return newMessage;
-        } else {
-          localStorage.setItem(
-            "oldMessages",
-            JSON.stringify({ [room]: newMessage })
-          );
-          return newMessage;
-        }
-      });
-    });
-  }, []);
+		socket.on('message', (message) => {
+			setMessages((messages) => {
+				let newMessage = [ ...messages, message ]
+				const oldMessages = JSON.parse(localStorage.getItem('oldMessages'))
+				if (oldMessages) {
+					localStorage.setItem('oldMessages', JSON.stringify({ ...oldMessages, [room]: newMessage }))
+					return newMessage
+				} else {
+					localStorage.setItem('oldMessages', JSON.stringify({ [room]: newMessage }))
+					return newMessage
+				}
+			})
+		})
+	}, [])
 
-  const sendMessage = (event) => {
-    event.preventDefault();
+	const sendMessage = (event) => {
+		event.preventDefault()
 
-    if (message) {
-      socket.emit("sendMessage", message, () => setMessage(""));
-    }
-  };
+		if (message) {
+			socket.emit('sendMessage', message, () => setMessage(''))
+		}
+	}
 
-  return (
-    <div className="outerContainer shawdow-2xl chat_box">
-      <span />
-      <span />
-      <span />
-      <span />
-      <div className="container">
-        <InfoBar room={room} />
-        <Messages messages={messages} name={name} />
-        <Input
-          message={message}
-          setMessage={setMessage}
-          sendMessage={sendMessage}
-        />
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className='outerContainer shawdow-2xl chat_box'>
+			<span />
+			<span />
+			<span />
+			<span />
+			<div className='container'>
+				<InfoBar room={room} />
+				<Messages messages={messages} name={name} />
+				<Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+			</div>
+		</div>
+	)
+}
 
-export default Chat;
+export default Chat
